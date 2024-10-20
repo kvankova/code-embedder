4e98016927b71ba69ad9b119b1b5b2ff0e20a528
Katerina Vankova <katerinavankova1@gmail.com>
2024-10-20 21:14:37 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 set -xe 
 git config --global --add safe.directory /github/workspace
 git update-index --chmod=+x entrypoint.sh
+git config --global user.name "github-actions"
+git config --global user.email "github-actions@github.com"
 git remote set-url origin https://x-access-token:$EMBED_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git fetch origin 
 
@@ -17,15 +19,14 @@ git checkout $BRANCH_NAME
 git pull
 
 # Check if there are changes to commit
-if [ -n "$(git status -s)" ]; then  # Use [ ] instead of [[ ]]
-    git config --global user.name "github-actions"
-    git config --global user.email "github-actions@github.com"
+if [ -n "$(git status -s)" ]; then  
     git add "$README_PATH"    
     git diff --staged
+
     echo "Pushing changes..."
     
     git commit -m "Update $README_PATH"
-    git push origin HEAD:refs/heads/$BRANCH_NAME
+    git push
 else
     echo "No changes to commit."
 fi
