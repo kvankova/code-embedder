20dad51c34cebb6fac58e83d5dd0058f54066f5f
Katerina Vankova <katerinavankova1@gmail.com>
2024-10-27 23:24:17 +0100

    feat: Update README_PATHS to all markdown files

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,9 @@ git config --global user.name "github-actions"
 git config --global user.email "github-actions@github.com"
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 
-README_PATHS=$1
+README_PATHS=$(find . -name "*.md" -print)
+
+echo "Found markdown files: $README_PATHS"
 
 BRANCH_NAME=${GITHUB_HEAD_REF:-$GITHUB_REF_NAME}
 
@@ -17,10 +19,10 @@ poetry run python /app/src/main.py --readme-paths $README_PATHS
 if [ -n "$(git status -s)" ]; then
 
     echo "Changes to commit..."
-    git add $README_PATHS
-    git diff --staged
-    git commit -m "Update $README_PATHS"
-    git push origin ${BRANCH_NAME}
+    #git add $README_PATHS
+    #git diff --staged
+    #git commit -m "Update $README_PATHS"
+    #git push origin ${BRANCH_NAME}
 else
     echo "No changes to commit."
 fi
