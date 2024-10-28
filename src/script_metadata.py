56f6e2dbf58281ac5e5ecb5a986bcc5f10b085cf
Katerina Vankova <katerinavankova1@gmail.com>
2024-10-28 22:45:39 +0100

    test: Refactor test for ScriptMetadataExtractor

--- src/script_metadata.py
+++ src/script_metadata.py
@@ -6,5 +6,5 @@ class ScriptMetadata:
     readme_start: int
     readme_end: int
     path: str
-    extraction_part: str | None = None  # None for full script, section_name for section
+    extraction_part: str | None = None
     content: str = ""
--- tests/test_script_metadata_extractor.py
+++ tests/test_script_metadata_extractor.py
@@ -1,154 +1,93 @@
 import pytest
 
-from src.code_embedding import ScriptMetadata
+from src.script_metadata import ScriptMetadata
 from src.script_metadata_extractor import ScriptMetadataExtractor
+from tests.conftest import create_script_metadata
+
+test_cases = [
+    pytest.param(
+        ["```python:main.py", "print('Hello, World!')", "```"],
+        [create_script_metadata(0, 2, "main.py")],
+        id="one_tagged_script",
+    ),
+    pytest.param(["```", "print('Hello, World!')", "```"], [], id="one_untagged_script"),
+    pytest.param([], [], id="empty_readme"),
+    pytest.param(
+        ["```javascript", "console.log('Hello, World!');", "```"],
+        [],
+        id="one_untagged_script_language_specified",
+    ),
+    pytest.param(
+        [
+            "```python:example.py",
+            "import os",
+            "print('Hello, World!')",
+            "```",
+            "```",
+            "print('Do not replace')",
+            "```",
+        ],
+        [create_script_metadata(0, 3, "example.py")],
+        id="one_tagged_script_one_untagged_script",
+    ),
+    pytest.param(
+        [
+            "```python:main.py",
+            "print('Hello, World!')",
+            "```",
+            "```python:example.py",
+            "import os",
+            "print('Hello, World!')",
+            "```",
+            "```",
+            "print('Do not replace')",
+            "```",
+        ],
+        [
+            create_script_metadata(0, 2, "main.py"),
+            create_script_metadata(3, 6, "example.py"),
+        ],
+        id="two_tagged_scripts_one_untagged_script",
+    ),
+    pytest.param(
+        ["```python:main.py:section_name", "print('Hello, World!')", "```"],
+        [create_script_metadata(0, 2, "main.py", "section_name")],
+        id="one_tagged_script_with_section_name",
+    ),
+    pytest.param(
+        [
+            "```python:main.py:section_name",
+            "print('Hello, World!')",
+            "```",
+            "```python:example.py",
+            "print('Hello, World!')",
+            "```",
+        ],
+        [
+            create_script_metadata(0, 2, "main.py", "section_name"),
+            create_script_metadata(3, 5, "example.py"),
+        ],
+        id="one_tagged_script_with_section_name_one_tagged_script",
+    ),
+    pytest.param(
+        [
+            "```python:main.py:A",
+            "print('Hello, World!')",
+            "```",
+            "```python:example.py:B",
+            "print('Hello, World!')",
+            "```",
+        ],
+        [
+            create_script_metadata(0, 2, "main.py", "A"),
+            create_script_metadata(3, 5, "example.py", "B"),
+        ],
+        id="two_tagged_scripts_with_section_names",
+    ),
+]
 
 
-@pytest.mark.parametrize(
-    "readme_content, expected",
-    [
-        (
-            ["```python:main.py", "print('Hello, World!')", "```"],
-            [
-                ScriptMetadata(
-                    readme_start=0,
-                    readme_end=2,
-                    path="main.py",
-                    extraction_part=None,
-                    content="",
-                )
-            ],
-        ),
-        (["```", "print('Hello, World!')", "```"], []),
-        ([], []),
-        (["```python", "print('Hello, World!')", "```"], []),
-        (
-            [
-                "```python:example.py",
-                "import os",
-                "print('Hello, World!')",
-                "```",
-                "```",
-                "print('Do not replace')",
-                "```",
-            ],
-            [
-                ScriptMetadata(
-                    readme_start=0,
-                    readme_end=3,
-                    path="example.py",
-                    extraction_part=None,
-                    content="",
-                )
-            ],
-        ),
-        (
-            [
-                "```python:main.py",
-                "print('Hello, World!')",
-                "```",
-                "```python:example.py",
-                "import os",
-                "print('Hello, World!')",
-                "```",
-                "```",
-                "print('Do not replace')",
-                "```",
-            ],
-            [
-                ScriptMetadata(
-                    readme_start=0,
-                    readme_end=2,
-                    path="main.py",
-                    extraction_part=None,
-                    content="",
-                ),
-                ScriptMetadata(
-                    readme_start=3,
-                    readme_end=6,
-                    path="example.py",
-                    extraction_part=None,
-                    content="",
-                ),
-            ],
-        ),
-        (
-            ["```python:main.py:section_name", "print('Hello, World!')", "```"],
-            [
-                ScriptMetadata(
-                    readme_start=0,
-                    readme_end=2,
-                    path="main.py",
-                    extraction_part="section_name",
-                    content="",
-                )
-            ],
-        ),
-        (
-            [
-                "```python:main.py:section_name",
-                "print('Hello, World!')",
-                "```",
-                "```python:example.py",
-                "print('Hello, World!')",
-                "```",
-            ],
-            [
-                ScriptMetadata(
-                    readme_start=0,
-                    readme_end=2,
-                    path="main.py",
-                    extraction_part="section_name",
-                    content="",
-                ),
-                ScriptMetadata(
-                    readme_start=3,
-                    readme_end=5,
-                    path="example.py",
-                    extraction_part=None,
-                    content="",
-                ),
-            ],
-        ),
-        (
-            [
-                "```python:main.py:A",
-                "print('Hello, World!')",
-                "```",
-                "```python:example.py:B",
-                "print('Hello, World!')",
-                "```",
-            ],
-            [
-                ScriptMetadata(
-                    readme_start=0,
-                    readme_end=2,
-                    path="main.py",
-                    extraction_part="A",
-                    content="",
-                ),
-                ScriptMetadata(
-                    readme_start=3,
-                    readme_end=5,
-                    path="example.py",
-                    extraction_part="B",
-                    content="",
-                ),
-            ],
-        ),
-    ],
-    ids=[
-        "one_tagged_script",
-        "one_untagged_script",
-        "empty_readme",
-        "one_untagged_script_language_specified",
-        "one_tagged_script_one_untagged_script",
-        "two_tagged_scripts_one_untagged_script",
-        "one_tagged_script_with_section_name",
-        "one_tagged_script_with_section_name_one_tagged_script",
-        "two_tagged_scripts_with_section_names",
-    ],
-)
+@pytest.mark.parametrize("readme_content, expected", test_cases)
 def test_script_path_extractor(
     readme_content: list[str], expected: list[ScriptMetadata]
 ) -> None:
