9acb9c776bdedaaa078f238ddd91630843aee7d6
Katerina Vankova <katerinavankova1@gmail.com>
2024-11-04 22:37:44 +0100

    refactor: Add o: and s: for object and section recognition

--- src/script_content_reader.py
+++ src/script_content_reader.py
@@ -37,14 +37,14 @@ class ScriptContentReader:
 
     def _process_scripts(self, scripts: list[ScriptMetadata]) -> list[ScriptMetadata]:
         full_scripts = [script for script in scripts if not script.extraction_part]
-        scripts_with_extraction_part = [script for script in scripts if script.extraction_part]
+        scripts_with_extraction = [script for script in scripts if script.extraction_part]
 
-        if scripts_with_extraction_part:
-            scripts_with_extraction_part = self._update_script_content_with_extraction_part(
-                scripts_with_extraction_part
+        if scripts_with_extraction:
+            scripts_with_extraction = self._update_script_content_with_extraction_part(
+                scripts_with_extraction
             )
 
-        return full_scripts + scripts_with_extraction_part
+        return full_scripts + scripts_with_extraction
 
     def _update_script_content_with_extraction_part(
         self, scripts: list[ScriptMetadata]
@@ -63,27 +63,50 @@ class ScriptContentReader:
     def _extract_part(self, script: ScriptMetadata) -> str:
         lines = script.content.split("\n")
 
-        # Try extracting as object first, then fall back to section
-        is_object, start, end = self._find_object_bounds(script)
-        if is_object:
-            return "\n".join(lines[start:end])
+        if script.extraction_type == "object":
+            start, end = self._extract_object_part(script)
 
-        # Extract section if not an object
-        start, end = self._find_section_bounds(lines)
-        if not self._validate_section_bounds(start, end, script):
-            return ""
+        elif script.extraction_type == "section":
+            start, end = self._extract_section_part(lines)
+            if not self._validate_section_bounds(start, end, script):
+                return ""
 
-        return "\n".join(lines[start:end])
-
-    def _validate_section_bounds(
-        self, start: int | None, end: int | None, script: ScriptMetadata
-    ) -> bool:
         if not start and not end:
             logger.error(
                 f"Part {script.extraction_part} not found in {script.path}. Skipping."
             )
-            return False
+            return ""
 
+        return "\n".join(lines[start:end])
+
+    def _extract_object_part(self, script: ScriptMetadata) -> tuple[int | None, int | None]:
+        tree = ast.parse(script.content)
+
+        for node in ast.walk(tree):
+            if (
+                isinstance(node, ast.FunctionDef)
+                | isinstance(node, ast.AsyncFunctionDef)
+                | isinstance(node, ast.ClassDef)
+            ):
+                if script.extraction_part == getattr(node, "name", None):
+                    start = getattr(node, "lineno", None)
+                    end = getattr(node, "end_lineno", None)
+                    return start - 1 if start else None, end
+
+        return None, None
+
+    def _extract_section_part(self, lines: list[str]) -> tuple[int | None, int | None]:
+        for i, line in enumerate(lines):
+            if re.search(self._section_start_regex, line):
+                start = i + 1
+            elif re.search(self._section_end_regex, line):
+                return start, i
+
+        return None, None
+
+    def _validate_section_bounds(
+        self, start: int | None, end: int | None, script: ScriptMetadata
+    ) -> bool:
         if not start:
             logger.error(
                 f"Start of section {script.extraction_part} not found in {script.path}. "
@@ -99,30 +122,3 @@ class ScriptContentReader:
             return False
 
         return True
-
-    def _find_section_bounds(self, lines: list[str]) -> tuple[int | None, int | None]:
-        for i, line in enumerate(lines):
-            if re.search(self._section_start_regex, line):
-                start = i + 1
-            elif re.search(self._section_end_regex, line):
-                return start, i
-
-        return None, None
-
-    def _find_object_bounds(
-        self, script: ScriptMetadata
-    ) -> tuple[bool, int | None, int | None]:
-        tree = ast.parse(script.content)
-
-        for node in ast.walk(tree):
-            if (
-                isinstance(node, ast.FunctionDef)
-                | isinstance(node, ast.AsyncFunctionDef)
-                | isinstance(node, ast.ClassDef)
-            ):
-                if script.extraction_part == getattr(node, "name", None):
-                    start = getattr(node, "lineno", None)
-                    end = getattr(node, "end_lineno", None)
-                    return True, start - 1 if start else None, end
-
-        return False, None, None
